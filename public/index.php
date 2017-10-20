d23a931b95b4ae2b8833ece54b818fcbecc71269
FoXFTW <hannes@octofox.de>
2017-10-20 17:31:09 +0200

    :hammer: load App to handle Requests

--- public/index.php
+++ public/index.php
@@ -1,3 +1,9 @@
 <?php
 
 require __DIR__.'/../vendor/autoload.php';
+
+$app = new \App\LaunchPad\Application(realpath(__DIR__.'/../'));
+
+$response = $app->handle(\App\LaunchPad\Request\Request::capture());
+
+$response->send();
