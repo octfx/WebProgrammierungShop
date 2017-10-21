7e3c7b7a793de07d339dc5bb2679d07d464e534f
FoXFTW <hannes@octofox.de>
2017-10-21 16:59:55 +0200

    :sparkles: Add helper functions; Load file via composer

---
+++ app/SparkPlug/helper.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: Hannes
+ * Date: 21.10.2017
+ * Time: 16:57
+ */
+
+if (!function_exists('str_contains')) {
+    function str_contains($haystack, $needle)
+    {
+        return strpos($haystack, $needle) !== false;
+    }
+}
