dc2db674f654ec848608a55bf89149e0ea6490d1
FoXFTW <hannes@octofox.de>
2017-10-23 22:34:44 +0200

    :hammer: Make Application Singleton, bind other Singletons into App; Add Helper-Function to resolve App

--- app/SparkPlug/Application.php
+++ app/SparkPlug/Application.php
@@ -7,16 +7,43 @@
 
 namespace App\SparkPlug;
 
-use App\SparkPlug\Interfaces\ResponseInterface;
+use App\SparkPlug\Response\ResponseInterface;
 use App\SparkPlug\Request\Request;
 
 class Application
 {
+    private static $instance;
     private $basePath;
+    private $resolvedClasses = [];
 
     public function __construct(string $basePath)
     {
         $this->basePath = $basePath;
+
+        static::$instance = $this;
+    }
+
+    public static function getInstance(): Application
+    {
+        if (is_null(static::$instance)) {
+            return new Application(__DIR__);
+        }
+
+        return static::$instance;
+    }
+
+    public function singleton(string $className): void
+    {
+        $this->resolvedClasses[$className] = new $className();
+    }
+
+    public function make(string $className, ?bool $new = false)
+    {
+        if (isset($this->resolvedClasses[$className]) && !$new) {
+            return $this->resolvedClasses[$className];
+        }
+
+        return new $className();
     }
 
     public function handle(Request $request): ResponseInterface
--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = new Application(realpath(__DIR__.'/../'));
 
+$app->singleton(\App\SparkPlug\Routing\Route::class);
+
 $response = $app->handle(Request::capture());
 
 $response->send();
