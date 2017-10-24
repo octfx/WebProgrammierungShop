f13846eaa6544fcd5f4992fc660646cf3a4485f6
FoXFTW <hannes@octofox.de>
2017-10-24 21:50:21 +0200

    ToDo: load app in separate File; set ExceptionHandler on load

--- public/index.php
+++ public/index.php
@@ -2,9 +2,11 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
+set_exception_handler([\App\SparkPlug\Exceptions\Handler::class, "handleException"]);
+
 $app = new Application(realpath(__DIR__.'/../'));
 
-$app->singleton(\App\SparkPlug\Routing\Route::class);
+$app->singleton(\App\SparkPlug\Routing\Router::class);
 
 $response = $app->handle(Request::capture());
 
