87e2266fbba8ffed3ceb4899a15fe8e6ae7c6c04
Nastassia <nast.heumann@ostfalia.de>
2017-11-14 18:52:16 +0100

    Profile view added

--- resources/assets/js/ria_details.js
+++ resources/assets/js/ria_details.js
@@ -6,28 +6,36 @@ var spanBewertung = modalBewertung.getElementsByClassName("close")[0];
 var saveBtnBewertung = document.getElementById("modal-bewertung-save");
 var deleteBtnBewertung = document.getElementById("modal-bewertung-delete");
 
-btnBewertung.onclick = function() {
+btnBewertung.onclick = function () {
     modalBewertung.style.display = "block";
 };
 
-spanBewertung.onclick = function() {
+spanBewertung.onclick = function () {
     modalBewertung.style.display = "none";
 };
 
-window.onclick = function(event) {
+window.onclick = function (event) {
     if (event.target == modalBewertung) {
         modalBewertung.style.display = "none";
     }
 };
 
-saveBtnBewertung.onclick = function() {
+saveBtnBewertung.onclick = function () {
     /* TODO save */
     modalBewertung.style.display = "none";
 };
 
-deleteBtnBewertung.onclick = function() {
-    /* TODO delete */
-   modalBewertung.style.display = "none";
+deleteBtnBewertung.onclick = function () {
+    // reset selected stars
+    var selectedStarLabels = modalBewertung.getElementsByClassName('label-rating selected');
+    for (var i = 0; i < selectedStarLabels.length; i++) {
+        selectedStarLabels[i].classList.remove('selected');
+    }
+
+    // reset description content
+    document.getElementById("modal-bewertung-description").value = "";
+
+    modalBewertung.style.display = "none";
 };
 
 /* --------------- Bearbeiten Modal --------------- */
