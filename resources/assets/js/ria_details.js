bba7edaa98568c33040152e50eefe020d98a0601
Nastassia <nast.heumann@ostfalia.de>
2017-11-14 17:28:00 +0100

    Modal für ria bewerten view mit funktion für editable star rating hinzugefügt

--- resources/assets/js/ria_details.js
+++ resources/assets/js/ria_details.js
@@ -1,25 +1,67 @@
-// Get the modal
-var modal = document.getElementById('modal-bewertung');
+/* --------------- Bewertung Modal --------------- */
 
-// Get the button that opens the modal
-var btn = document.getElementById("btn-bewertung");
+var modalBewertung = document.getElementById('modal-bewertung');
+var btnBewertung = document.getElementById("btn-bewertung");
+var spanBewertung = modalBewertung.getElementsByClassName("close")[0];
+var saveBtnBewertung = document.getElementById("modal-bewertung-save");
+var deleteBtnBewertung = document.getElementById("modal-bewertung-delete");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+btnBewertung.onclick = function() {
+    modalBewertung.style.display = "block";
+};
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+spanBewertung.onclick = function() {
+    modalBewertung.style.display = "none";
+};
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modalBewertung) {
+        modalBewertung.style.display = "none";
     }
-}
+};
+
+saveBtnBewertung.onclick = function() {
+    /* TODO save */
+    modalBewertung.style.display = "none";
+};
+
+deleteBtnBewertung.onclick = function() {
+    /* TODO delete */
+   modalBewertung.style.display = "none";
+};
+
+/* --------------- Bearbeiten Modal --------------- */
+
+/*
+var modalBearbeiten = document.getElementById('modal-bearbeiten');
+var btnBearbeiten = document.getElementById("btn-bearbeiten");
+var spanBearbeiten = modalBearbeiten.getElementsByClassName("close")[0];
+var saveBtnBearbeiten = document.getElementById("modal-bearbeiten-save");
+var deleteBtnBearbeiten = document.getElementById("modal-bearbeiten-delete");
+
+btnBearbeiten.onclick = function() {
+    modalBearbeiten.style.display = "block";
+};
+
+spanBearbeiten.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target == modalBearbeiten) {
+        modalBearbeiten.style.display = "none";
+    }
+};
+
+saveBtnBearbeiten.onclick = function() {
+    // TODO save
+    modalBearbeiten.style.display = "none";
+};
+
+deleteBtnBearbeiten.onclick = function() {
+    // TODO delete
+    modalBearbeiten.style.display = "none";
+};
+
+*/
+
--- resources/assets/js/stars.js
+++ resources/assets/js/stars.js
@@ -1,14 +1,31 @@
-var editableStarRating = document.getElementsByClassName('editable-star-rating');
-for(var i = 0; i < editableStarRating.length; i++) {
-    var inputs = editableStarRating[i].getElementsByClassName('input');
-    for(var j = 0; j < inputs.length; j++) {
-        inputs[i].addEventListener('change', function () {
+var editableStarRatings = document.getElementsByClassName('editable-star-rating');
+
+for(var i = 0; i < editableStarRatings.length; i++) {
+    var inputs = editableStarRatings[i].getElementsByClassName('input-rating');
+
+    for (var j = 0; j < inputs.length; j++) {
+        inputs[j].addEventListener('change', function () {
             var radio = this;
-            var selectedStars = document.getElementsByClassName('rating').getElementsByClassName('selected');
-            for (var i = 0; i < selectedStars.length; i++) {
-                selectedStars[i].classList.remove('selected');
+            var selectedStarLabels = document.getElementsByClassName('label-rating selected');
+            for (var i = 0; i < selectedStarLabels.length; i++) {
+                selectedStarLabels[i].classList.remove('selected');
             }
-            radio.closest('label').addClass('selected');
+            var label = closestNodeWithClass(radio, 'label-rating');
+            if(label != null) {
+                label.classList.add('selected');
+            }
         });
     }
+}
+
+var closestNodeWithClass = function(startElement, nodeClass) {
+    var el = startElement;
+
+    while (el.className != nodeClass) {
+        el = el.parentNode;
+        if (!el) {
+            return null;
+        }
+    }
+    return el;
 }
