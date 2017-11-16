7a0aa719778d7d1675c3b77099cab0ea52f9f97b
Nastassia <nast.heumann@ostfalia.de>
2017-11-17 00:41:26 +0100

    Search function implemented, gallery design changed, size and layout of basic template elements changed, start page changed, profile page modified and more

---
+++ resources/assets/js/galerie.js
@@ -0,0 +1,15 @@
+document.getElementById("ria-gallery-search-input").oninput = function () {
+    var searchInput = this.value.toLowerCase();
+
+    var galleryEntries = document.getElementsByClassName("gallery-entry");
+
+    for(var i = 0; i < galleryEntries.length; i++) {
+        var riaName = galleryEntries[i].getElementsByClassName("ria-name")[0].innerHTML.toLowerCase();
+        if(riaName.indexOf(searchInput) == -1) { // ria name does not contain search input as substring
+            galleryEntries[i].style.display = "none";
+        } else {
+            galleryEntries[i].style.display = "inline-block";
+        }
+    }
+
+}
