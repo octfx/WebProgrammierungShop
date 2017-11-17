b7de80963235e798d77cd771370eaa2700c2ccd4
FoXFTW <hannes@octofox.de>
2017-11-18 00:05:02 +0100

    :bug: Fix Testing; bootstrap App in config/app.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,10 @@
-<?php use App\SparkPlug\Application, App\SparkPlug\Request\Request, \App\SparkPlug\Exceptions\Handler;
+<?php declare(strict_types = 1); use App\SparkPlug\Request\Request, \App\SparkPlug\Exceptions\Handler;
 
 require __DIR__.'/../vendor/autoload.php';
 
 set_exception_handler([Handler::class, "handleException"]);
 
-$app = new Application(realpath(__DIR__.'/../'));
-
-$app->singleton(\App\SparkPlug\Routing\Router::class);
-$app->loadRoutes();
+$app = require __DIR__.'/../config/app.php';
 
 $response = $app->handle(Request::capture());
 
