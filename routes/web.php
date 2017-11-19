bc1621c91c43ec3e74690cfae2fca06a0139352d
FoXFTW <hannes@octofox.de>
2017-11-19 20:05:52 +0100

    :fire: Cleanup web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php $router = app()->make(\App\SparkPlug\Routing\Router::class);
 
-//Router::get('/login', 'UserController@login')->name('login');
 $router->get('/', 'PageController@showIndexView')->name('index');
 $router->get('/login', 'User\AccountController@showLoginView')->name('login');
 $router->get('/register', 'User\AccountController@showRegisterView')->name('register');
 $router->get('/gallery', 'Ria\GalleryController@showGalleryView')->name('gallery');
-$router->get('/ria/[0]', 'Ria\RiaController@showRiaDetailsView')->name('riaDetails'); // TODO add ria name or id as param in path
+$router->get('/ria/[0]', 'Ria\RiaController@showRiaDetailsView')->name('riaDetails');
 $router->get('/profile', 'Profile\ProfileController@showProfileView')->name('profile');
-
