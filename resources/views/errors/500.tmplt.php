9b174acc35cc95165814ced2eb33b5e4a79ac8e5
FoXFTW <hannes@octofox.de>
2017-12-06 12:14:26 +0100

    :bug: Fix 500.tmplt.php breaking the app

--- resources/views/errors/500.tmplt.php
+++ resources/views/errors/500.tmplt.php
@@ -1,15 +1,18 @@
-@use('app')
-@set('title', '500 - Interner Fehler')
-@set('content')
+<html>
+<head>
+    <title>500 - Interner Fehler</title>
+</head>
+<body>
 <div class="container">
     <div class="contentbox jumbotron">
         <h1>Interner Serverfehler!</h1>
         <div class="error" style="text-align: left;">
-        <?php if (config('app.debug')) {
-            echo $error;
-        }
-        ?>
+            <?php if (config('app.debug')) {
+                echo $error;
+            }
+            ?>
         </div>
     </div>
 </div>
-@endset
+</body>
+</html>
